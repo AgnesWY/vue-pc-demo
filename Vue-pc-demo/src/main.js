89bf6ffb2b7898626eeb57655381d092b4e7c646
wangyue <411489690@qq.com>
2019-08-20 19:43:16 +0800

    添加全局样式文件

--- Vue-pc-demo/src/main.js
+++ Vue-pc-demo/src/main.js
@@ -3,6 +3,11 @@ import App from './App'
 import router from './router'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 引入自定义的全局样式
+// import "normalize.css"; // normalize.css 样式格式化
+import "@/assets/styles/index.scss"; // 全局自定义的css样式
+import '@/assets/styles/css/self.css'
+
 Vue.use(Element);
 
 Vue.config.productionTip = false

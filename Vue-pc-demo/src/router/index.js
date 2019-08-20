743ebe20d557e483c094f2a539f4aa7b7b1fffd7
wangyue <411489690@qq.com>
2019-08-20 18:22:42 +0800

    项目建立

---
+++ Vue-pc-demo/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const _import = require('./_import_'+ process.env.NODE_ENV)
+
+
+Vue.use(Router)
+
+export const constantRouterMap = [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: _import('start')
+  },
+  // 多层路由时，需要一个layout支撑你的父级路由
+  {
+
+  }
+]
+
+export default new Router({
+  // 路由页面切换的时候，存在页面 滑动问题，这个方法 将页面的定位恢复
+  scrollBehavior: () => ({
+    y: 0
+  }),
+  routes: constantRouterMap
+})
---
+++ Vue-pc-demo/src/views/start.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <hello-world></hello-world>
+</div>
+  
+</template>
+
+<script>
+import helloWorld from '@/components/HelloWorld'
+export default {
+    components:{
+        helloWorld
+    }
+
+}
+</script>
+
+<style>
+
+</style>
